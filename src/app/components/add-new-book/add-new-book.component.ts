7bb62e2eea06425a1d5b562baaed07395f173424
@black.monster.maker <black.monster.maker@gmail.com>
2023-08-13 23:51:03 +0700

    add new book page

--- src/app/components/add-new-book/add-new-book.component.ts
+++ src/app/components/add-new-book/add-new-book.component.ts
@@ -1,25 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { IBook } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-add-new-book',
   templateUrl: './add-new-book.component.html',
   styleUrls: ['./add-new-book.component.css'],
 })
-export class AddNewBookComponent {
+export class AddNewBookComponent implements OnInit {
+  allBooks: IBook[] = [];
+
+  bookCategory: Observable<string> | undefined;
+
+  constructor(private bookService: BookService) {}
+
   addNewBookForm!: FormGroup;
 
   ngOnInit(): void {
-    this.submitAddNewBook();
-  }
+    this.getAllBooks();
 
-  submitAddNewBook() {
     this.addNewBookForm = new FormGroup({
       title: new FormControl(null, [Validators.required, Validators.email]),
       author: new FormControl(null, [Validators.required]),
       category: new FormControl(null, [Validators.required]),
       description: new FormControl(null, [Validators.required]),
     });
+  }
+
+  getAllBooks() {
+    // this.bookService
+    //   .getAllBooks()
+    //   .pipe(
+    //     map((books: IBook[]) =>
+    //       books.map((book) => console.log('Book category : ', book))
+    //     )
+    //   );
+    this.bookCategory = this.bookService.getAllBooks().pipe(
+      map((book: any) => {
+        console.log(book);
+        return book.category;
+      })
+    );
+  }
+
+  submitAddNewBook() {
     console.log('Add New Book : ', this.addNewBookForm);
   }
 }
